409082829d2092ef49c5ab1ddc1ba4b984ce8053
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-09-02 11:42:41 +0900

    NoteEditingControllerルーティングした

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/NoteEditing','NoteEditingController@PushEditedData');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
