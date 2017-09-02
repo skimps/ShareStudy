079e33b9fb2cdbda955403bc60447363fc2d0e30
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-09-03 04:24:59 +0900

    ああああ

--- app/Http/Controllers/NoteListShowController.php
+++ app/Http/Controllers/NoteListShowController.php
@@ -11,6 +11,15 @@ class NoteListShowController extends Controller
     //クラスの全てのNoteリストを得る
     public function GetAllNoteInClass($class_id){
       $note_list= Note::where('class_id',$class_id)->get();
+      foreach ($note_list as $item)
+          echo $item->text.'<br>';
+      //TODO
+      //Viewを返す
+    }
+    //指定のノートを得る
+    public function GetNote($note_id){
+      $note= Note::where('id',$note_id)->first();
+          echo $note->text.'<br>';
       //TODO
       //Viewを返す
     }
