7b19d1c302c6aa60a2de7d6e3d851e01cef55d5d
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-09-03 00:55:52 +0900

    コメント追加

--- app/Http/Controllers/NoteFavoriteController.php
+++ app/Http/Controllers/NoteFavoriteController.php
@@ -17,20 +17,28 @@ class NoteFavoriteController extends Controller
     
     //ふぁぼる
     public function SetFavoritedNote($user_id,$note_id){
+        //例外判定
         if(Note::where('note_id',$note_id)->count()==0)
             throw new Exception("id={$note_id}の行は存在しません!!error!!!<br>");
         if(Favorite::where('note_id',$note_id)->where('user_id',$user_id)->count()!=0)
             throw new Exception("とりあえず例外吐いておく");
+        
+        //ふぁぼリストに追加
         Favorite::create(['note_id'=>$note_id,'user_id'=>$user_id]);
+        
         //TODO
         //viewを返す
     }
     
         //ふぁぼる解除
     public function UnSetFavoritedNote($favorite_id){
+        //例外判定
         if(Favorite::where('id',$favorite_id)->count()==0)
             throw new Exception("id={$favorite_id}の行は存在しません!!error!!!<br>");
+        
+        //ふぁぼリストから削除
         Favorite::where('id',$favorite_id)->delete();
+        
         //TODO
         //viewを返す
     }
