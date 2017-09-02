2e5ac7e2733c01d68e3188eebba6d209d30371f5
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-09-02 11:25:59 +0900

    NoteEditingControllerをとりあえず作成

---
+++ app/Http/Controllers/NoteEditingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+//ノート編集ページのコントローラ
+class NoteEditingController extends Controller
+{
+    //編集したノートを使ってデータベースへプッシュする
+    public function PushEditedData(){
+        
+    }
+}
