0a62eabc117d8fa3e93f26682c2ae63a848d9235
fuchi15 <sakuya815@gmail.com>
2017-09-02 15:35:17 +0900

    confirect

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', 'timetableController@show');
-
-Route::get('/','')
-
-
-
-
-=======
-=======
->>>>>>> 7078dd0dfdeaf29eefb7997593abf1f2407f21ba
 <?php
 
 /*
@@ -45,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/NoteEditing','NoteEditingController@PushEditedData');
+Route::get('/', 'timetableController@show');
 
 Auth::routes();
 
